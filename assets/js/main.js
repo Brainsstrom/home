37b0f0f498a3b2363df8de99c8c2e7f62fe1b7bc
Brainsstrom <brainsstrom.wit.me@gmail.com>
2024-06-06 22:02:50 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,7 +195,7 @@ const sr = ScrollReveal({
 sr.reveal(`.home__title1, .home__title2, .home__img, .about__data, .instructor_bg_img, .section-header, .swiper, .contact__img`)
 sr.reveal(`.instructor__container, .instructor__img5, .title3, .contact__title, .contact__description`, {delay: 500})
 sr.reveal(`.about__img, .home__title2, .social-icons-home, .home__buttons_2, .instructor__data, .faq-item`, {delay: 500, interval: 100})
-sr.reveal(`.what-you-will-learn-list, .scrolldownbtn, .show__buttons1, .contact__buttons, .scrollup_button, .swiper-pagination`, {interval: 100})
+sr.reveal(`.what-you-will-learn-box, .scrolldownbtn, .contact__buttons, .scrollup_button, .swiper-pagination`, {interval: 100})
 
 /*=============== SHOW HEADER & HIDE HEADER ===============*/
 let lastScrollTop = 0;
